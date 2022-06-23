edbc9717aaef563f3821109f913bfad22b99ad99
Here <tgb3308@naver.com>
2022-06-23 18:15:43 +0900

    2022.06.23

---
+++ jv201/src/main/java/com/varxyzjv200/mod002/Number.java
@@ -0,0 +1,33 @@
+package com.varxyzjv200.mod002;
+
+public class Number {
+//	public static void main(String[] args) {
+//		int i;
+//		int j;
+//		for ( i=2; i<10; i++) {
+//			for (j=1; j<10; j++) {
+//				System.out.printf(i+"*" +j+ "=" +i*j+"\t");
+//			}
+//		}
+//	}
+	
+//	public static void main(String[] args) {
+//		int num,count,i;
+//		for (num=2; num <= 1000; num++) {
+//			count=0;
+//			for (i=1; i<=num; i++) {
+//				if(num%i==0)
+//					count= count+1;
+//				
+//			}
+//			if(count==2) {
+//				System.out.println(num);
+//			}
+//		}
+//	}
+	
+	public static boolean main(String[] args) {
+		String text[] = {"The cat in the hat"};
+		return false;
+	}
+}
